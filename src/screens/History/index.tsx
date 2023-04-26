6be867350d60b7bbb839aa0e52084d46599945b8
johnnysantos <johnny.santos@bairesdev.com.br>
2023-04-26 18:38:56 -0300

    Close Swipeable via ref

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { View, ScrollView, Alert, Pressable } from 'react-native'
 import { HouseLine, Trash } from 'phosphor-react-native';
@@ -19,6 +19,8 @@ export function History() {
 
   const { goBack } = useNavigation();
 
+  const swipeableRefs = useRef<Swipeable[]>([])
+
   async function fetchHistory() {
     const response = await historyGetAll();
     setHistory(response);
@@ -31,7 +33,8 @@ export function History() {
     fetchHistory();
   }
 
-  function handleRemove(id: string) {
+  function handleRemove(id: string, index: number) {
+    swipeableRefs.current?.[index].close()
     Alert.alert(
       'Remover',
       'Deseja remover esse registro?',
@@ -67,7 +70,7 @@ export function History() {
         showsVerticalScrollIndicator={false}
       >
         {
-          history.map((item) => (
+          history.map((item, index) => (
             <Animated.View
               key={item.id}
               entering={SlideInRight}
@@ -75,10 +78,15 @@ export function History() {
               layout={Layout.springify()}
             >
               <Swipeable
+                ref={(ref) => {
+                  if(ref) {
+                    swipeableRefs.current.push(ref)
+                  }
+                }}
                 overshootLeft={false}
                 containerStyle={styles.swipeableContainer}
                 renderLeftActions={() => (
-                  <Pressable style={styles.swipeableRemove} onPress={() => handleRemove(item.id)}>
+                  <Pressable style={styles.swipeableRemove} onPress={() => handleRemove(item.id, index)}>
                     <Trash size={32} color={THEME.COLORS.GREY_100} />
                   </Pressable>
                 )}
