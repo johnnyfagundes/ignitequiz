f301f39134b0416258666dcf235b83ddc5c118c0
johnnysantos <johnny.santos@bairesdev.com.br>
2023-04-26 18:29:55 -0300

    Remove using Swipleable

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { View, ScrollView, TouchableOpacity, Alert } from 'react-native';
-import { HouseLine } from 'phosphor-react-native';
+import { View, ScrollView, Alert, Pressable } from 'react-native'
+import { HouseLine, Trash } from 'phosphor-react-native';
+import Animated, { Layout, SlideInRight, SlideOutRight } from 'react-native-reanimated'
 
 import { Header } from '../../components/Header';
 import { HistoryCard, HistoryProps } from '../../components/HistoryCard';
@@ -9,6 +10,8 @@ import { HistoryCard, HistoryProps } from '../../components/HistoryCard';
 import { styles } from './styles';
 import { historyGetAll, historyRemove } from '../../storage/quizHistoryStorage';
 import { Loading } from '../../components/Loading';
+import { Swipeable } from 'react-native-gesture-handler'
+import { THEME } from '../../styles/theme'
 
 export function History() {
   const [isLoading, setIsLoading] = useState(true);
@@ -65,12 +68,25 @@ export function History() {
       >
         {
           history.map((item) => (
-            <TouchableOpacity
+            <Animated.View
               key={item.id}
-              onPress={() => handleRemove(item.id)}
+              entering={SlideInRight}
+              exiting={SlideOutRight}
+              layout={Layout.springify()}
             >
-              <HistoryCard data={item} />
-            </TouchableOpacity>
+              <Swipeable
+                overshootLeft={false}
+                containerStyle={styles.swipeableContainer}
+                renderLeftActions={() => (
+                  <Pressable style={styles.swipeableRemove} onPress={() => handleRemove(item.id)}>
+                    <Trash size={32} color={THEME.COLORS.GREY_100} />
+                  </Pressable>
+                )}
+              >
+                <HistoryCard data={item} />
+              </Swipeable>
+
+            </Animated.View>
           ))
         }
       </ScrollView>
